94a837a5fd2eb66162f449c73572f61a4bb2947d
amrut <amrut@192.168.1.213>
2022-12-10 14:29:56 +0530

    First commit

---
+++ JenkinsDemoApp/src/main/java/com/example/demo/JenkinsDemoAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoAppApplication.class, args);
+		System.out.println("Hello.. from JenkinsDemoApp!!");
+	}
+
+}
