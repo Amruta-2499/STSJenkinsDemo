e70d539330e9050dc9b94178bb8e4bf089b56b42
amrut <amrut@Shree>
2022-12-12 11:46:20 +0530

    Third commit

--- JenkinsDemoApp/src/main/java/com/example/demo/JenkinsDemoAppApplication.java
+++ JenkinsDemoApp/src/main/java/com/example/demo/JenkinsDemoAppApplication.java
@@ -9,6 +9,7 @@ public class JenkinsDemoAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoAppApplication.class, args);
 		System.out.println("Hello... from JenkinsDemoApp!!");
+		System.out.println("Greetings!!......");
 	}
 
 }
